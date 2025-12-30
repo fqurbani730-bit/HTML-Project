df3d9d17fab703e96192ba8f2714a1a06e2111fb
fqurbani730-bit <fqurbani730@gmail.com>
2025-12-30 10:07:27 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,875 @@
+// document.querySelector("h1").innerHTML="hello";
+// let username="ali\'s pen"
+// console.log(username);
+// window.alert("say hello to world")
+// document.write("say hello to the world")
+// document.querySelector("h1").innerHTML="hellooo";
+// console.log("document.queryselectorh");
+/*Javascript due to finding alittle monye.*/ 
+// let start=true;
+// let end=false;
+// console.log(start);
+// console.log(typeof start);
+// console.log(end);
+// console.log(typeof end);
+// let x;
+// console.log(x);
+// console.log(typeof x);
+// let y=null;
+// console.log(y);
+// console.log(typeof y);
+// let Username='F';
+// let uSername=`while your are studing JS for first time, you may thing you dont know anythinglike\'s`;
+// console.log(uSername);
+// const radial=2.5;
+// let number1=23;
+// let number2=30;
+// console.log(number1==number2);
+// console.log(number1===number2);
+
+// let a= false;
+// console.log(a);
+// console.log(typeof a);
+// let b= 0;
+// console.log(0);
+// console.log(typeof b);
+
+// let DBpassword=12345;
+// let userpassword=+prompt("enter your password");
+// DBpassword=== userpassword? console.log("welcome"):console.log("invalid value");
+
+// let oredr=["cheese","sandwich","cola","soup"]
+// console.log(oredr.length);
+
+// let name=[];
+// name[0]=23;
+// name[1]="ali";
+// name[0]=45;
+// console.log(name[0]);
+// console.log(name[1]);
+// let Name= new Array( 1,2,3,4,5);
+// Name[0]="ahmad";
+// console.log(Name);
+
+// let Afg={
+//     capital:"kabul",
+//     bigcity:"herat",
+//     smalcity:"ghore",
+// }
+// Afg.beautiful="pangshir";
+// console.log(Afg.capital);
+// console.log(Afg["bigcity"]);
+// let afg={}
+// afg=34;
+// afg="ali";
+// console.log(afg);
+// let AfG=new Object(12,true,"passed");
+// console.log(AfG);
+// class AFg{
+//     constructor(capital, bigcity,smalcity,beautiful){
+//         this ,capital="kabul";
+//         this ,bigcity="herat";
+//         this ,smalcity="ghore";
+//         this ,beautiful="pangshir";
+
+//     }
+// }
+// console.log(AFg);
+// operators
+1
+//  let number1=40;
+// let number2= 50;
+// number2=number2+number2;
+// number2+=number2;
+// console.log(number2);
+// number1=number1-number1;
+// number1-=number1;
+// console.log(number1);
+// number1*=number1;
+// console.log(number1);
+// number1/=number1;
+// console.log(number1);
+// number1%=number1;
+// console.log(number1);
+
+
+// number2++;
+// number2--;
+// console.log(number2);
+// console.log(number1+number2);
+// console.log(number2-number1);
+// console.log(number1*number2);
+// console.log(number2/number1);
+// console.log(number2%number1);
+// console.log(number2** number1);
+// console.log(number2==number1);
+// console.log(number2=== number1);
+// console.log(number2!=number1);
+// console.log(number2>=number1);
+// console.log(number2>number1);
+// console.log(number2<=number1);
+// console.log(number2<number1);
+// let numbers= 10;
+// for(let control=1; control<=10; control++){
+//     console.log(control);
+// }
+// let Number=0;
+// for(let con=0; con<=20; con+2){
+    
+// }
+// console.log(Number);
+
+// let plus=0;
+// for(let index=1; index<=50; index++){
+//     plus+=index;
+   
+// }
+// console.log(plus);
+// let username ="Fatema";
+// for(let i=0 ;i < username.length; i++ ){
+//     console.log(username[i]);
+// }
+// let user="kowsar";
+// for(index=0; index< user.length; index++){
+//     console.log(user[index]);
+// }
+// let text=`this will be my first practice of javascript`
+
+// for(app=0; app<text.length; app++){
+//     console.log(text[app]);
+// }
+// let list=["egg","potato", "milk"]
+// for(i=0;i<list.length; i++){
+//     console.log(list[i])
+
+// }
+let biggest=[40, 50, 65,75,3,10,4]
+let result=0;
+// for(i=0; i<biggest.length ; i++){
+//     if( biggest[i]>10){
+//         result++;
+//     }
+// }
+// console.log(result)
+// for(i=0; i<biggest.length; i++){
+//     if(biggest[i]<10){
+//         result++;
+//     }
+// }
+// console.log(result)
+// let password=1234;
+// let userpassword=+prompt("enter your password");
+// let interpassword=3;
+// let user="ali";
+
+// if(password!== userpassword || password>password.length){
+//     console.log("error");
+// }
+// -?-:-;
+// let scores= +prompt("enteryour score")
+// if(scores>=90){console.log("A")} else if(scores>=70 && scores<=89){console.log("B")} else(scores<70);{console.log("C")};
+// let score=85;
+// if(score>=85){console.log("A")} else if(score>=70){console.log("B")} else{console.log("C")};
+// let scor=90;
+// if(scor>=90){console.log("A")} else if(scor>=80){console.log("B")} else if(scor>=70){console.log("C")} else{console.log("Fail")};
+// let numbers=[3,8,12,5,7];
+// // let i=0;
+// for(  let i=0; i<numbers.length ;i+2){
+//    console.log(i);
+   
+//     i++;
+// };
+
+// function sum(...args){
+//     let total=0;
+//     for(let n of args){
+//         total+=n;
+//     }
+    
+//     console.log(total)
+    
+// }
+// sum(1 ,4,3,6 ,5,1 ,67);
+// let form=[{
+//     name:"sara", lastname:"ahmadi", age:23,
+// },
+// {name:"ahmad", lastname:"noree", age:22,},
+// {name:"rasol", lastname:"qurbani", age:10,}
+// ];
+// let i=0;
+
+
+// do{ form[i].name=== name && form.age===age} while(
+//     !form.name===name && !form.age===age
+
+// ){
+//   console.log("compelet");  
+// };
+   
+// let form;
+// let compelet;
+// do{
+//     form={
+//         name:"ali",lastname:"ahmadi",agr:23
+//     };
+//     compelet= true;
+// } while(compelet!=false){
+//     console.log("not compelet")
+// };
+let form = [
+  { name: "sara", lastname: "ahmadi", age: 23 },
+  { name: "ahmad", lastname: "noree", age: 22 },
+  { name: "rasol", lastname: "qurbani", age: 10 }
+];
+
+// let name = "ahmad";
+// let age = 22;
+
+// let i = 0;
+// let found = false;
+// let form1=form[i];
+
+// do {
+//   if (form[i].name === name && form[i].age === age) {
+//     console.log("complete");
+//     found = true;
+//     break;
+//   }
+//   i++;
+// } while (i < form.length);
+
+// if (!found) {
+//   console.log("not found");
+// }
+
+
+
+
+// let form1 = [
+//   { name: "sara", lastname: "ahmadi", age: 23 },
+//   { name: "ahmad", lastname: "noree", age: 22 },
+//   { name: "rasol", lastname: "qurbani", age: 10 }
+// ];
+// let name="sara";
+// let lastname="ahmadi";
+// let age=23;
+// let i=0;
+// let found=true;
+// let form2=form1[i];
+// console.log(form2.name);
+
+// do{if(form2.name=== name && form2.lastname=== lastname && form2.age===age)
+//     {console.log("you compelet the form");
+//     found=true;
+//     break;
+
+
+// }
+// i++;
+
+
+// }
+// while(i<form1.length);{
+//     if (!found){
+//         console.log("not found");
+
+// }}
+// ;
+
+// let contects=[{name:"ali",phone:1234 ,status:"online"},
+//     {name:"ma",phone:123445 ,status:"offline"},
+//     {name:"ta",phone:123468 ,status:"offine"},
+//     {name:"trr",phone:1234899 ,status:"offline"},
+// ]
+// for(x of contects){
+//     console.log(x);
+// }
+
+// let i=0;
+ 
+// while(i<contects.length){
+//     let contect=contects[i];
+//     if(contect.status==="online"){
+//         console.log(" send massage");
+//     }
+//     else{
+//         console.log("don\'t sent massage"+ "  "+  contect.name);
+//     }
+//     i++;
+
+// }
+// let leader=[
+//     {name:"ali", cart:true,},
+//     {name:"ali", cart:false},
+//     {name:"ali", cart:true},
+//     {name:"ali", cart:false},
+// ];
+// let i=0;
+// let regester=0;
+// let focus;
+// for(i=0; i<leader.length; i++){
+//     if(leader[i].cart===true){
+//         console.log("refeuse");
+//     }
+
+// };
+// while(i<leader.length){
+//     if(leader[i].cart===false){
+//         regester+=cart=regester
+        
+//     }
+//     i++;
+// }
+
+// let order=[
+//     {
+//         price:30, paid:true, delivered:true, customertype:"vip"
+//     },
+//      {
+//         price:20, paid:false, delivered:true, customertype:"normal"
+//     },
+//      {
+//         price:10, paid:true, delivered:false, customertype:"vip"
+//     },
+//      {
+//         price:40, paid:false, delivered:false, customertype:"normal"
+//     },
+// ];
+// let being=100;
+// let sendermony=50;
+// let index=0;
+
+// while(index<order.length){
+//     if(order[index].paid===true &&order[index].delivered===false && order[index].price>0){
+//         console.log("valid");
+//     }
+//     else if(order[index].customertype==="vip"){
+//         console.log("sent without any beats");
+//     }
+//     else if(order[index].customertype==="normal"){
+//         if(sendermony>=order[index].price){
+//             console.log("sending");
+//         }
+//         else{console.log("don\'t send");}
+//     }
+//     i++;
+// };
+
+
+// started from here
+// let name="ali";
+// console.log(name);
+// function print(username){
+//     console.log(username+"hello my hony");
+// }
+// const user=function(name, callback){
+//     callback(name);
+// }
+// user(name,print);
+
+
+// let age=prompt("enter your age");
+
+// function print(userage){
+//     console.log(userage);
+   
+// }
+// function adult(age,callback){
+//     callback(age);
+//      if(age<18 ){
+//         callback("you are child")
+//     }
+//     else{
+//         console.log("you are adult")
+//     };
+// }
+// adult(age,print);
+// let multi=[2,3,6,9,90];
+
+
+// function multipale(mult,callback){
+//     callback(mult);
+// };
+
+// function process(mult){
+    
+//      console.log(mult*2);
+
+    
+// }
+// let i=0;
+// for(i=0;i<multi.length;i++){
+//     multipale(multi[i] ,process);
+   
+// }
+// let users=["ali","mahmod","sara"
+// ];
+
+// function username(user,callback){
+//     callback(user);
+// }
+// function massage(user){
+    
+//     if(user==="ali"){console.log("hello boss"+user)}
+//     else if(user=== "mahmod"){console.log("go away"+" "+user)}
+//     else{console.log("welcome my dear"+" "+user)}
+   
+
+// }
+// let i=0;
+// for(i=0; i<users.length; i++){
+//     username(users[i],massage);
+// }
+
+
+// let numbers=[1,2,3,4,5];
+// function num(number,callback){
+//     callback(number);
+// }
+// function numb(numberss){
+//     if(numberss%2===0){
+//         console.log("couple");
+//     }
+//     else{
+//         console.log("add");
+//     }
+   
+
+// }
+// let index=0;
+// for(index=0; index<numbers.length;index++){
+//     num(numbers[index],numb);
+
+// }
+// let numbers=[1,2,3,4,5];
+// function num(number,callback){
+//     callback(number);
+
+// }
+// function numb(numberss){
+//     if(numberss%2===0 && numberss>3){
+//         console.log("special");
+//     }
+    
+//     else{
+//         console.log("add");
+//     }
+    
+   
+
+// }
+// let index=0;
+// for(index=0; index<numbers.length;index++){
+//     num(numbers[index],numb);
+
+// }
+// function outer(){
+//   let number=0;
+//   return function(){
+//     number++;
+//     console.log(number);
+//   }
+ 
+// }
+// let out = outer();
+// out();
+// out();
+// out();
+// function outer(){
+//   let number=0;
+//   return function(numbers){
+//     number+=numbers
+//     console.log(number)
+//   }
+// }
+// let total=outer();
+// total(23);
+// total(12);
+// total(60);
+// function outer(permision){
+//   let price=permision;
+//   return function(){
+//     if(price=== true){
+//       console.log("access")
+//     }
+//     else{
+//       console.log("not access")
+//     }
+
+//   }
+// }
+// let total=outer(true);
+// total();
+// total();
+// total();
+
+// function outer(){
+//    let Number=0;
+//    return function(){
+//     Number++
+//     console.log(Number+5);
+//    }
+// }
+// let end=outer();
+// end();
+// end();
+
+// let numb=[1,2,3,4]
+// function test(value){
+//   console.log(value);
+
+// }
+// function test1(numb,callback){
+//   callback(numb);
+// }
+
+// let i=0;
+// for(i=0; i<numb.length; i++){
+//   test(numb,test1)
+// // }
+// let order=[{
+//     id:1,user:"ali",items:[100,200,300],paid:true,statuss:"shipped",priority:"high"},
+//     {id:2,
+//     user:"mahmod",
+//     items:[
+//         100,400,300
+//     ],
+//     paid:true,
+//     statuss:"pending",
+//     priority:"normal"},
+//      {id:3,
+//     user:"sara",
+//     items:[
+//         100,400,300
+//     ],
+//     paid:true,
+//     statuss:"delivered",
+//     priority:"high"},
+// ];
+
+// let i=0;
+// while(i<order.length){let book= order[i];
+//     if(book.priority==="high" && book.paid===true &&book.statuss!=="delivered"){
+
+
+//         let total=fruits(book.items);
+        
+//             if(total>300){
+//                 console.log(book.user+"valid order");
+//             }
+//    i++     }
+//     }
+    
+
+// function fruits(items){
+//     let sum=0;
+//     let index=0;
+//     for(index=0;index<items.length;index++){
+//             sum+=items[index];
+//         }
+//             return sum;
+
+// }
+// let foodorder=[
+//     {id:1, user:"ali", items:[
+//         {name:"egg", price:100},
+//         { name:"milk",price:200},
+//         { name:"bread",price:400}
+        
+
+        
+//     ],paid:true,
+//     status:"pending",
+//     priority:"high"
+
+//     },
+//      {id:2, user:"ahmad", items:[
+//         { name:"egg", price:200,},
+//         { name:"milk",price:100,},
+//         {name:"bread",price:600}
+//     ],paid:true,
+//     status:"shipped",
+//     priority:"normal"
+
+//     },
+//      {id:3, user:"sara", items:[
+//         {name:"egg", price:10,},
+//         {name:"milk",price:20,},
+//         { name:"bread",price:40}
+//     ],paid:false,
+//     status:"canceled",
+//     priority:"normal"
+
+//     },
+//      {id:4, user:"farid", items:[
+//         {name:"egg", price:100,},
+//         {name:"milk",price:200,},
+//         {name:"bread",price:400}
+//     ],paid:true,
+//     status:"delevired",
+//     priority:"high"
+
+//     }
+// ];
+// let i=0;
+// while(i<foodorder.length){
+//     let food=foodorder[i];
+//     if(food.priority==="high" && food.status!=="delevired"&& food.paid===true)
+//         {
+//         let S=0;
+//         // let egg=false;
+//         for(S=0; S<food.items.length; S++){
+//        if (food.items[S].name ==="egg")
+//         {
+        
+//         // egg=true;
+//         // break;}
+//         // }
+    
+//         // if(egg){
+        
+//             let total=  order(food.items);
+//             if(total>200){
+//                 console.log(`valid order ragestered by ${food.user} with ${food.id}` );
+//             }
+       
+//    }}
+// }
+// i++
+    
+// }
+
+// function order(items){
+//     let sum=0;
+//     let index=0;
+//     while(index<items.length){
+//         sum+=items[index].price;
+//         index++;
+//     }
+//     return sum;
+    
+// }
+// let raw = "      fatemeh _qurbani           ";
+// raw=raw.trim();
+// let search=raw.indexOf("_");
+// console.log(search);
+
+// let first=raw.replace(/ _.*/," ");
+// console.log(first);
+// let firstlitter= "F".concat(first.slice(1));
+// console.log(firstlitter);
+// if(search!==-1){
+//     first=raw.slice(0,search);
+
+// }
+// let final=first.charAt(0). toUpperCase().concat(first.slice(1));
+// console.log(final);
+
+// let rawname="    mahdi.     ";
+// rawname = rawname.trim();
+// console.log(rawname);
+// let findindex= rawname.indexOf(".");
+
+// console.log(findindex);
+// let slic=rawname.slice(0,length-1);
+// console.log(slic);
+// let partthree;
+// if(findindex!==-1){
+//     partthree=rawname.slice(0,findindex);
+// }
+// let partfour=partthree.charAt(0).toUpperCase().concat(partthree.slice(1));
+// console.log(partfour);
+
+// let way = rawname.replace("mahdi.","Mahdi");
+// console.log(way);
+
+// let username="     ali    ";
+// username=username.trim();
+// console.log(username);
+// let last= username.slice(0,username.length-1);
+// console.log(last);
+// let lastlitter=username.charAt(username.length-1);
+// console.log(lastlitter);
+// let finalstep=rest
+
+// let total =username.slice(0,-1)+username.charAt(username.length-1).toUpperCase();
+// console.log(total);
+// let username="    ali...   ";
+// username=username.trim();
+// let replace1 =username.replace("ali...","ali");
+// console.log(replace1);
+//  let date ="    sara__ahmadi....__   ";
+//  date =date.trim();
+//  date=date.replace("....","");
+//  date=date.replaceAll("__","");
+
+//  console.log(date);
+
+
+// let input ="    ali__reza__karimi...__      ";
+// input=input.trim();
+// input=input.replace("...","");
+// input=input.replaceAll("__"," ");
+// console.log(input);
+
+
+// let data="    ali,ahmadi,079956465"   ;
+// let result1=data.split(",");
+// console.log(result)
+// ;
+// let biglitter=data.charAt(0);
+// let firt=data.slice(1) ;
+// console.log(biglitter);
+// let capital= biglitter.toUpperCase().concat();
+// console.log(capital);
+
+// let user="   john|  -3500   ";
+// user=user.trim();
+// let spl=user.split("|");
+// let firstpart=spl[0].trim();
+// let secondpart=+spl[1].trim();
+// console.log( typeof secondpart);
+// let has=false;
+// let i=0;
+// for(i=0; i<firstpart.length ;i++){
+//   let first=firstpart[i];
+//   if(first>="0" && first<="9"){
+//     has =true;
+//   }
+// }
+// console.log(has)
+
+//   if( !has && secondpart> 0){
+//     console.log("valid");
+//   }
+//   else{ console.log("invalid")};
+
+
+
+// let user="  ali   |   -25678.75   ";
+// user=user.trim();
+// let spl=user.split("|");
+// let firstpart=spl[0].trim();
+// let secondpart= parseFloat(spl[1].trim());
+// secondpart=Math.abs(spl[1]);
+
+
+// let has=false;
+// let i=0;
+// for(i=0; i<firstpart.length; i++){
+//   if(firstpart[i]>"0" && firstpart[i]<"9"){
+//     has=true;
+//   }
+// }
+// if(!has && secondpart>0){
+  
+// let secondpart2 =Math.floor(Math.random()*(15 - 5))+5;
+//  let secondpart3= secondpart*secondpart2/100;
+// let finalprice= secondpart-secondpart3;
+// finalprice= Math.round(finalprice*100)/100;
+// console.log("valid order");console.log("discount:",secondpart2+"%");
+// console.log(finalprice.toFixed(2));
+
+// }
+// else{console.log("invali")}
+
+
+// let onlineorder=[
+//   { id:1,totalprice:45000,paystatuse:true,sendstatuse:false,items:[3000,20000,34000]},
+// { id:2,totalprice:35000,paystatuse:false,sendstatuse:true,items:[6000,50000,38000]},
+// {id:3,totalprice:25000,paystatuse:true,sendstatuse:false,items:[3000,90000,3000]}
+
+// ];
+// console.log(onlineorder);
+// let valid=0;
+// for(let i=0;i<onlineorder.length; i++){
+//   let order=onlineorder[i];
+//   if(order.paystatuse===true&&order.sendstatuse===false&&order.totalprice>=0){
+//     let sum=0;
+//     for(let index=0; index<order.items.length; index++){
+//       if(order.items[index]>=20000){
+//         sum+=order.items[index];
+      
+//       }
+//     }
+//      if (sum>0){
+//     console.log("valid"+"   "+ order.id);
+//      valid++;
+//   }else{console.log("invalid")};}
+ 
+
+// };
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
